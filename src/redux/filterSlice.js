5db085fa9a8df30f0c0744b57fd124690fc868de
galynary <107271972+galynary@users.noreply.github.com>
2023-04-10 19:28:01 +0300

    change

--- src/redux/filterSlice.js
+++ src/redux/filterSlice.js
@@ -4,7 +4,7 @@ const filterSlice = createSlice({
   name: 'filter',
   initialState: '',
   reducers: {
-    setFilterValue(action) {
+    setFilterValue(state, action) {
       return action.payload;
     },
   },
